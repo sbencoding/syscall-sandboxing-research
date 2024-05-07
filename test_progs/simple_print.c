ce8d8aad430f6c31c899618e6452c3bf9712a3c9
Beni Selyem <b.selyem@student.tudelft.nl>
2024-05-07 16:35:38 +0200

    feat: Initial tracer code
    Simple single process, single thread tracer
    Can capture all syscalls and list name, number, return value
    Simple makefile to build the tracer and test programs
    Add first simple test program for the tracer

---
+++ test_progs/simple_print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hi there!\n");
+    return 0;
+}
---
+++ tracer.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <linux/ptrace.h> // glibc doesn't necessarily expose the ptrace_syscall_info struct
+#include <sys/wait.h>
+
+#include "sysnr_map.h" // map syscall numbers to their names
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fputs("Please provide at least the path of the executable to trace!\n", stderr);
+        return 1;
+    }
+
+    printf("Going to trace: %s\n", argv[1]);
+    pid_t child_pid = fork();
+    if (child_pid == 0) {
+        printf("Child requesting trace by the parent");
+        ptrace(PTRACE_TRACEME, NULL, NULL, NULL);
+        execve(argv[1], &argv[2], NULL);
+    } else {
+        printf("Starting up the tracer\n");
+        int wstatus;
+        int child_pid;
+        struct ptrace_syscall_info syscall_info;
+
+        while ((child_pid = waitpid(-1, &wstatus, 0)) != -1) {
+            if (WIFSIGNALED(wstatus)) {
+                // TODO: we might need to continue (forward signal) the process here for some signals
+                printf("We are stopping because of a signal: %d\n", WSTOPSIG(wstatus));
+            } else if (WIFEXITED(wstatus)) {
+                printf("Child process exited\n");
+            } else if (WIFSTOPPED(wstatus)) {
+                int stop_sig = WSTOPSIG(wstatus);
+                switch (stop_sig) {
+                    case SIGTRAP:
+                        // TODO: PTRACE_O_TRACEEXEC
+                        ptrace(PTRACE_SETOPTIONS, child_pid, NULL, PTRACE_O_TRACESYSGOOD);
+                        ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL);
+                        break;
+                    case SIGTRAP | 0x80:
+                        ptrace(PTRACE_GET_SYSCALL_INFO, child_pid, sizeof(syscall_info), &syscall_info);
+                        switch (syscall_info.op) {
+                            case PTRACE_SYSCALL_INFO_ENTRY:
+                                printf("SYS_%s [%lu] @ %#08lx", sysnr_map[syscall_info.entry.nr], syscall_info.entry.nr, syscall_info.instruction_pointer);
+                                break;
+                            case PTRACE_SYSCALL_INFO_EXIT:
+                                printf(" -> %ld\n", syscall_info.exit.rval);
+                                break;
+                            case PTRACE_SYSCALL_INFO_SECCOMP:
+                                puts("This syscall contains seccomp info, not yet handled by the tracer");
+                                break;
+                            case PTRACE_SYSCALL_INFO_NONE:
+                                puts("This syscall contains no data");
+                                break;
+                        }
+                        ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL);
+                        break;
+                    default:
+                        // TODO: we might need to continue the process here for some signals
+                        fputs("Error: signal is not yet handled by the tracer!\n", stderr);
+                        break;
+                }
+            }
+        }
+    }
+}

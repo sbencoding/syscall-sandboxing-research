6843414d1e6950189c6e74d364bada002863272a
Beni Selyem <b.selyem@student.tudelft.nl>
2024-06-19 09:19:47 +0200

    test: Add multi phase support
    Add multi phase support to the case runner framework.
    Add multi phase test cases for the simple programs.

--- interact/anl_simple_progs.py
+++ interact/anl_simple_progs.py
@@ -8,9 +8,27 @@ class AnlSimplePrint(ProgAnalysis):
     def case_default(self):
         self.exec()
 
+class AnlSimplePrintMP(ProgAnalysis):
+    def __init__(self):
+        super(AnlSimplePrintMP, self).__init__('test_progs_out/simple_print')
+        self.enable_multi_phase([0x1147])
+
+    @input_case
+    def case_default(self):
+        self.exec()
+
 class AnlForking(ProgAnalysis):
     def __init__(self):
         super(AnlForking, self).__init__('test_progs_out/forking')
+
+    @input_case
+    def case_default(self):
+        self.exec()
+
+class AnlForkingMP(ProgAnalysis):
+    def __init__(self):
+        super(AnlForkingMP, self).__init__('test_progs_out/forking')
+        self.enable_multi_phase([0x118f])
 
     @input_case
     def case_default(self):
@@ -24,6 +42,15 @@ class AnlMultiForking(ProgAnalysis):
     def case_default(self):
         self.exec()
 
+class AnlMultiForkingMP(ProgAnalysis):
+    def __init__(self):
+        super(AnlMultiForkingMP, self).__init__('test_progs_out/multi_forking')
+        self.enable_multi_phase([0x1175, 0x1183])
+
+    @input_case
+    def case_default(self):
+        self.exec()
+
 class AnlMultiPthread(ProgAnalysis):
     def __init__(self):
         super(AnlMultiPthread, self).__init__('test_progs_out/multi_pthread')
@@ -32,9 +59,27 @@ class AnlMultiPthread(ProgAnalysis):
     def case_default(self):
         self.exec()
 
+class AnlMultiPthreadMP(ProgAnalysis):
+    def __init__(self):
+        super(AnlMultiPthreadMP, self).__init__('test_progs_out/multi_pthread')
+        self.enable_multi_phase([0x11db])
+
+    @input_case
+    def case_default(self):
+        self.exec()
+
 class AnlSimpleExecve(ProgAnalysis):
     def __init__(self):
         super(AnlSimpleExecve, self).__init__('test_progs_out/simple_execve')
+
+    @input_case
+    def case_default(self):
+        self.exec()
+
+class AnlSimpleExecveMP(ProgAnalysis):
+    def __init__(self):
+        super(AnlSimpleExecveMP, self).__init__('test_progs_out/simple_execve')
+        self.enable_multi_phase([0x11b3])
 
     @input_case
     def case_default(self):
--- interact/common.py
+++ interact/common.py
@@ -9,17 +9,35 @@ def input_case(func):
 input_case.idx = 0
 
 class ProgAnalysis():
-    def __init__(self, prog_path, opts=[]):
+    def __init__(self, prog_path, opts=None):
         self.prog_path = prog_path
-        self.opts = opts
+        self.opts = [] if opts is None else opts
         self.syscalls_used = set()
         self.cases_ran = 0
+        self.phase_count = 0
+
+    def enable_multi_phase(self, offsets):
+        comb_args = zip(['-p' for _ in offsets], [hex(x) for x in offsets])
+        self.opts += [x for tup in comb_args for x in tup]
+        self.phase_count = len(offsets)
+        self.syscalls_used = {}
+        for i in range(0, self.phase_count+1):
+            self.syscalls_used[i] = set()
 
     def _process_proc_output(self, proc):
         stdout_data = proc.communicate()[0]
         lines = stdout_data.splitlines()
-        syscalls = lines[-1].decode()[:-1]
-        self.syscalls_used = self.syscalls_used.union(set(syscalls.split(',')))
+        if self.phase_count != 0:
+            syscalls_per_phase = [ln.decode()[:-1] for ln in lines[-(self.phase_count + 1):]]
+            for phase in syscalls_per_phase:
+                parts = phase.split(':')
+                if len(parts) != 2: continue # phase was not hit, no syscalls to add
+                idx, syscalls = parts
+                self.syscalls_used[int(idx)] |= set(syscalls.split(','))
+        else:
+            syscalls = lines[-1].decode()[:-1]
+            self.syscalls_used = self.syscalls_used.union(set(syscalls.split(',')))
+
         return b'\n'.join(lines[:-1])
 
     def exec(self):
@@ -52,4 +70,4 @@ class ProgAnalysis():
         print()
 
     def stats(self):
-        print(self.prog_path, f'; ran {self.cases_ran} times', '; used syscalls:', self.syscalls_used)
+        print(self.prog_path, f'; ran {self.cases_ran} times' + (' [MP]' if self.phase_count != 0 else ''), '; used syscalls:', self.syscalls_used)
